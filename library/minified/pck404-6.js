167b9e6420d8dcf5baeebc37b3b686483c8c4733
KOIII7337 <k7837267@gmail.com>
2026-03-09 19:20:13 -0400

    Create pck404-6.js

---
+++ library/minified/pck404-6.js
@@ -0,0 +1,1 @@
+(t==0||((floor(new Date().getTime()/250)%20)==0))&&(r=(a,b)=>floor(a+random()*(b-a)),M=r(5,9)*2,G=r(2,6)*4,m=new Array(M).fill(0).map(_=>r(2,18)),g=new Array(G).fill(0).map(_=>r(0,3)),b=1.2+random()*.8,k=1+random()*3,w=r(62,145),S=r(10,14),s=r(11,13)),t*=b,((t*m[(t>>S)%M]*g[(t>>s)%G%2])&w)|floor(sin(sqrt(t%4096)*k))
