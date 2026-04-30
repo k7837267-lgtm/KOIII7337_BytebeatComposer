074257c7f28bfa062cd1fc9f5254260df9ef0ef2
KOIII7337 <k7837267@gmail.com>
2026-04-29 20:52:33 -0400

    Update audioProcessor.mjs

--- scripts/audioProcessor.mjs
+++ scripts/audioProcessor.mjs
@@ -217,16 +217,9 @@ class audioProcessor extends AudioWorkletProcessor {
 						return outValue;
 					};
 					break;
-			case 'PIbeat':
-				this.getValues = (funcValue, ch) => {
-					const outValue = Math.max(Math.min(funcValue, Math.PI), (Math.PI * -1));
-					this.lastByteValue[ch] = Math.round((outValue + 1) * 127.5);
-					return outValue;
-				};
-				break;
 			case 'Inverted Bytebeat':
 				this.getValues = (funcValue, ch) =>
-					(this.lastByteValue[ch] = (funcValue - 1) * - 1 & 255) / 127.5 - 1;
+					(this.lastByteValue[ch] = (funcValue + 1) * - 1 & 255) / 127.5 - 1;
 				break;
 				default: this.getValues = (funcValue, ch) => (this.lastByteValue[ch] = NaN);
 			}
